e1c049ea2ad3e8073d53868dd23e7d87c552ca70
evarist1 <evaristchibueze@gmail.com>
2024-03-20 11:04:31 +0000

    coreection of task3

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,11 +1,5 @@
--- My SQL setup file for development stage of project
--- Requirements are:
--- creates a database called hbtn_test_db
--- creates a user called hbnb_test in localhost
--- sets the password of the hbnb_test user to hbnb_test_pwd
--- grants the user ALL privileges on the database hbnb_test_db and only this database
--- grants the user SELECT privileges on the database preformance_schema and only this database
--- if the database hbnb_test_db or user hbnb_test already exist the script should not fail
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+-- Prepares a MySQL server for the project.
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
